b3d603b670b86771f373d81b966f592b369bfd52
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2021-12-15 20:00:12 +0100

    Created Report Form

---
+++ PassBook/PassBook/Report.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace PassBook
+{
+    partial class Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dgvReportDetails = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReportDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Report Details";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(49, 98);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(79, 20);
+            this.dtpFrom.TabIndex = 1;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTo.Location = new System.Drawing.Point(146, 98);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(86, 20);
+            this.dtpTo.TabIndex = 2;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(258, 96);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(83, 28);
+            this.btnView.TabIndex = 3;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // dgvReportDetails
+            // 
+            this.dgvReportDetails.AllowUserToAddRows = false;
+            this.dgvReportDetails.AllowUserToDeleteRows = false;
+            this.dgvReportDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReportDetails.Location = new System.Drawing.Point(-3, 149);
+            this.dgvReportDetails.Name = "dgvReportDetails";
+            this.dgvReportDetails.ReadOnly = true;
+            this.dgvReportDetails.Size = new System.Drawing.Size(738, 219);
+            this.dgvReportDetails.TabIndex = 4;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(583, 390);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(111, 36);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "Export PDF";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dgvReportDetails);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.label1);
+            this.Name = "Report";
+            this.Text = "Report";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReportDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.DataGridView dgvReportDetails;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
